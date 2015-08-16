8ebb9517ef2f812005988b9c316cee5b2dfa67cf
Ray <pengrui1993@gmail.com>
2015-08-16 03:44:38 -0400

    update course json

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ function selectAll(id) {
 $(document).ready(function () {
 
     $(window).resize(layout);
-    $('#version').html('ver: ' + document.lastModified);
+    // $('#version').html('ver: ' + document.lastModified);
 
     coursesList = new CoursesList(document.getElementById("addedCoursesList"));
     calendar = new CalendarCollection(document.getElementById("calendarViews"), coursesList);
